c51a815b5c1714ed42dfb10ad3b06c5aefd2c74f
Vee-keme <veliaprecious@gmail.com>
2024-03-08 15:22:54 +0100

    update

--- src/components/SingleProject.jsx
+++ src/components/SingleProject.jsx
@@ -1,33 +1,37 @@
 import React from "react";
 import project1 from "../assets/project1.png";
+import { projectData } from "../projectsData";
 // import "../styles/Workpage.css";
 
 const SingleProject = () => {
+  console.log("this", projectData);
   return (
     <div className="single-project">
-      <h3 className="text-start my-6 text-4xl">Netflix Clone</h3>
-      <div className="projects__container">
-        <div className="projects--imageDiv">
-          <img src={project1} alt="project image" className="projects--image" />
-        </div>
+      {projectData.map((item, index) => (
+        <div>
+          <h3 className="text-start my-6 text-4xl">{item?.title}</h3>
+          <div className="projects__container">
+            <div className="projects--imageDiv">
+              <img
+                src={item?.image}
+                alt="project image"
+                className="projects--image"
+              />
+            </div>
 
-        <div className="projects--text">
-          <div>
-            <p>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt
-              corporis modi adipisci unde distinctio assumenda asperiores!
-              Sapiente eum quibusdam dignissimos. Lorem ipsum, dolor sit amet
-              consectetur adipisicing elit. Deserunt corporis modi adipisci unde
-              distinctio assumenda asperiores! Sapiente eum quibusdam
-              dignissimos.
-            </p>
+            <div className="projects--text">
+              <div>
+                <p>{item?.details}</p>
+                <p className="text-sm italic">{item?.Language}</p>
+              </div>
+              <button className="projects--text-button animate-pulse border rounded-lg px-4 py-2 mt-6">
+                Live Website
+                <span className="ml-4">→</span>
+              </button>
+            </div>
           </div>
-          <button className="projects--text-button animate-pulse border rounded-lg px-4 py-2 mt-6">
-            Live Website
-            <span className="ml-4">→</span>
-          </button>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/projectsData.js
+++ src/projectsData.js
@@ -1,12 +1,36 @@
+import netflixClone from "../src/assets/project1.png";
+import repoApp from "../src/assets/project2.png";
+import lightsOut from "../src/assets/project3.png";
+
 export const projectData = [
   {
-    id: 1,
-    name: "",
-    image: "",
-    liveLink: "",
-    githubCode: "",
-    Language: "",
-    details: "This app renders your githb repositories",
+    title: "Netflix Clone",
+    image: netflixClone,
+    liveLink: "#",
+    githubCode: "#",
+    Language: "React",
+    details:
+      "A react app that works with the netflix api and displays the latest and newest movies",
+    features: "",
+  },
+  {
+    title: "Repo App",
+    image: repoApp,
+    liveLink: "#",
+    githubCode: "#",
+    Language: "Vue",
+    details:
+      "Repoapp, built with vuejs, fetches all existing github repositories from a particular github's database and displays, showing the features, status, etc of the app",
+    features: "",
+  },
+  {
+    title: "Lights Out",
+    image: lightsOut,
+    liveLink: "#",
+    githubCode: "#",
+    Language: "React",
+    details:
+      "Built with react's class based components, lightsout is a game based on Tiger electronics game meant to show my overall understanding of react",
     features: "",
   },
 ];
